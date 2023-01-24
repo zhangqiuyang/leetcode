05de21e5d43bb785f1b953b0cbcfef4497f51cab
zhangqiuyang <360006813@qq.com>
2023-01-25 00:02:46 +0800

    Create Math_70_Climbing Stairs.java
    dp

---
+++ Math_70_Climbing Stairs.java
@@ -0,0 +1,24 @@
+class Solution {
+    // 备忘录
+    int[] memo;
+
+    public int climbStairs(int n) {
+        memo = new int[n + 1];
+        return dp(n);
+    }
+
+    // 定义：爬到第 n 级台阶的方法个数为 dp(n)
+    int dp(int n) {
+        // base case
+        if (n <= 2) {
+            return n;
+        }
+        if (memo[n] > 0) {
+            return memo[n];
+        }
+        // 状态转移方程：
+        // 爬到第 n 级台阶的方法个数等于爬到 n - 1 的方法个数和爬到 n - 2 的方法个数之和。
+        memo[n] = dp(n - 1) + dp(n - 2);
+        return memo[n];
+    }
+}
