ff241954afdf19ca47dad75f678e6fa91b16ca8c
zhangqiuyang <360006813@qq.com>
2023-02-13 17:17:07 +0800

    Create Tree_226_Invert Binary Tree.java

---
+++ Tree_226_Invert Binary Tree.java
@@ -0,0 +1,12 @@
+public TreeNode invertTree(TreeNode root) {
+        if(root != null){
+            TreeNode node = root.left;
+            root.left = root.right;
+            root.right = node;
+            
+            invertTree(root.left);
+            invertTree(root.right);
+        }
+        
+        return root;
+}
