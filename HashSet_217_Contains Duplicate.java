e415fde27e5806495dd812f2bffe54702d05538f
zhangqiuyang <360006813@qq.com>
2023-02-16 20:22:51 +0800

    Create HashSet_217_Contains Duplicate.java

---
+++ HashSet_217_Contains Duplicate.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            if(map.containsKey(nums[i])){
+                
+                return true;
+            }
+            map.put(nums[i],i);
+        }
+        return false;
+    }
+}
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
