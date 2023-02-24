fe9520d11fdc830090da4dd307fa9a0040aaf80a
zhangqiuyang <360006813@qq.com>
2023-02-24 21:22:06 +0800

    Create Dp_509_Fibonacci Number.java

---
+++ Dp_509_Fibonacci Number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        if (n == 0 || n == 1) {
+            return n;
+        }
+        int dp_i_1 = 1, dp_i_2 = 0;
+        for (int i = 2; i <= n; i++) {
+            // dp[i] = dp[i - 1] + dp[i - 2];
+            int dp_i = dp_i_1 + dp_i_2;
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i_1;
+    }
+}
